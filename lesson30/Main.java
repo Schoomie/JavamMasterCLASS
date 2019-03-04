55ebfbed78f733514a7780c58f7d8f9960c6a64d
Schoomie <jakubpatecki97@gmail.com>
2019-03-04 16:39:34 +0100

    "finito"

--- lesson30/Main.java
+++ lesson30/Main.java
@@ -4,4 +4,21 @@ package lesson30;
  * Created by Kuba on 04/03/2019.
  */
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+        System.out.println("Full name" + person.getFullName());
+        System.out.println("teen = " + person.isTeen());
+        person.setFirstName("John");
+        person.setAge(18);
+        System.out.println("full name:" + person.getFullName());
+        System.out.println("teen =" + person.isTeen());
+        person.setLastName("Smith");
+
+        System.out.println("full name:" + person.getFullName());
+
+
+    }
 }
--- lesson30/Person.java
+++ lesson30/Person.java
@@ -4,4 +4,63 @@ package lesson30;
  * Created by Kuba on 04/03/2019.
  */
 public class Person {
+    String firstName, lastName;
+    int age;
+
+    public String getFirstName(String firstName) {
+        return firstName;
+    }
+
+    public String getLastName(String lastName) {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+
+    }
+
+    public boolean isTeen() {
+        if (getAge() > 12 || getAge() < 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getFullName() {
+        if (getFirstName(firstName).isEmpty()) {
+            if (getLastName(lastName).isEmpty()) {
+
+            }
+            return lastName;
+        } else {
+            if (getLastName(lastName).isEmpty()) {
+                return firstName;
+            } else {
+                return firstName + " " + lastName;
+            }
+        }
+
+
+    }
+
+
 }
+
